aa691daf19da623d7dc57a7775d63d344cd3d30f
Cerek Hillen <chillen@lyft.com>
2020-09-27 23:55:36 -0400

    WIP breaking everything

--- graft/__init__.py
+++ graft/__init__.py
@@ -5,20 +5,32 @@ from typing import Any
 from typing import Callable
 from typing import Dict
 from typing import Generic
+from typing import List
+from typing import Optional
 from typing import Set
 from typing import TypeVar
 
-import gevent
+from gevent import Greenlet
 from gevent.pool import Pool
 
 
-NodeVal = TypeVar("NodeVal")
 T = TypeVar("T")
+U = TypeVar("U")
 
 
-class Node(Generic[NodeVal]):
-    def __init__(self, name: str, work: Callable[..., NodeVal]):
-        self.name = name
+class WorkItem(Generic[T], ABC):
+    @abstractmethod
+    def __call__(self, *args, **kwargs) -> T:
+        pass
+
+
+class Node(Generic[T], WorkItem[T]):
+    name: str
+    # intentionally left out, because mypy complains about being unable to assign to a method
+    # work: Callable[..., T]
+
+    def __init__(self, work: Callable[..., T], name: Optional[str] = None):
+        self.name = name if name else work.__name__
         self.work = work
 
     def __eq__(self, other: object) -> bool:
@@ -29,105 +41,179 @@ class Node(Generic[NodeVal]):
     def __hash__(self) -> int:
         return hash(self.name)
 
-    def __call__(self, *args, **kwargs) -> NodeVal:
+    def __call__(self, *args, **kwargs) -> T:
         return self.work(*args, **kwargs)
 
     def __repr__(self) -> str:
         return f"Node({self.name})"
 
 
-class Graph(Generic[T]):
+class BaseGraph(Generic[T]):
+    vertices: Set[T]
+    edges: Dict[T, Set[T]]
+
+    def __init__(self):
+        self.vertices = set()
+        self.edges = {}
+
+    def add_vertex(self, t: T) -> "BaseGraph[T]":
+        """
+        Adds a vertex to the graph.
+        """
+        self.vertices.add(t)
+        return self
+
+    def add_edge(self, from_vertex: T, to_vertex: T) -> "BaseGraph[T]":
+        """
+        Adds a directed edge from a particular vertex to another vertex.
+        """
+        if from_vertex not in self.vertices or to_vertex not in self.vertices:
+            raise KeyError()
+
+        self.edges[from_vertex].add(to_vertex)
+        return self
+
+    def add_edges(self, from_vertex: T, to_vertices: Set[T]) -> "BaseGraph[T]":
+        """
+        Adds directed edges from a particular vertex to many other vertices, using built-in set
+        operations as an accelerant.
+        """
+        if from_vertex not in self.vertices or not (to_vertices & self.vertices):
+            raise KeyError()
+
+        if from_vertex not in self.edges:
+            self.edges[from_vertex] = set()
+        self.edges[from_vertex] |= to_vertices
+        return self
+
+    def topological_ordering(self) -> List[T]:
+        """
+        Produces the topological ordering for the nodes contained in this graph. If a cycle is
+        detected, then this throws an exception instead.
+        """
+        # TODO(crockeo): implement topological sort
+        return []
+
+
+class Graph(Generic[T], WorkItem[T]):
     nodes: Dict[str, Node]
-    dependencies: Dict[Node, Set[Node]]
+    graph: BaseGraph[Node]
+    topological_ordering: List[Node]
+    terminal_node: Optional[Node[T]]
 
     def __init__(self):
         self.nodes = {}
-        self.dependencies = {}
-        self.terminal_node = None
+        self.graph = BaseGraph()
+        self.topological_ordering = []
 
-    def node(self, *, terminal=False) -> Callable[[Callable[..., NodeVal]], Node[NodeVal]]:
-        def _node(work: Callable[..., NodeVal]) -> Node[NodeVal]:
-            name = work.__name__
-            dependencies = set()
+    # TODO(crockeo): typing
+    def node(self, *, name: Optional[str] = None, terminal: bool = False):
+        """
+        A decorator that maps a function onto its corresponding Node while registering it onto the
+        graph and recomputing the topological ordering. Example:
 
-            args = inspect.getfullargspec(work)[0]
-            for arg in args:
+        graph = Graph()
+
+        @graph.node()
+        def work() -> int:
+            return 5
+
+        @graph.node(terminal=True)
+        def work2(work: int) -> int:
+            return 2 * work
+
+        graph()  # 10
+        """
+
+        def _node(work: Callable) -> Node:
+            n = Node(work, name)
+
+            dependencies: Set[Node] = set()
+            for arg in inspect.getfullargspec(work)[0]:
                 if arg not in self.nodes:
-                    # TODO(crockeo): populate this with something meaningful
+                    # TODO(crockeo): raise better
                     raise KeyError()
                 dependencies.add(self.nodes[arg])
 
-            n = Node(work.__name__, work)
-            self.nodes[name] = n
-            self.dependencies[n] = dependencies
             if terminal:
                 if self.terminal_node is not None:
-                    # TODO(crockeo): more informative error
+                    # TODO(crockeo): raise better
                     raise Exception()
+                    # if not isinstance(n, Node[T]):
+                    #     # TODO(crockeo): raise better
+                    raise Exception()
+
                 self.terminal_node = n
+
+            self.nodes[n.name] = n
+            self.graph.add_vertex(n)
+            self.graph.add_edges(n, dependencies)
 
             return n
 
         return _node
 
+    def __call__(self, *args, **kwargs) -> T:
+        """
+        Executes all of the Nodes contained in this graph asynchronously (ordering according to
+        their dependencies).
+        """
+        if self.terminal_node is None:
+            # TODO: raise better
+            raise Exception()
 
-class Executor(ABC):
-    @abstractmethod
-    def __call__(self, graph: Graph[T]) -> T:
-        pass
-
-
-class GreenletExecutor(Executor):
-    pool: Pool
-
-    def __init__(self, worker_count: int):
-        self.pool = gevent.pool.Pool(worker_count)
-
-    def __call__(self, graph: Graph[T]) -> T:
-        # FYI: if you're reading this I know that this section of code is abysmal, and I just
-        # haven't gotten around to fixing it yet. Don't worry :)
-
+        pool = Pool(5)  # TODO(crockeo): parametrize worker count
         return_values: Dict[Node, Any] = {}
-        not_run: Set[Node] = set(graph.nodes.values())
-        can_run = [node for node in not_run if not graph.dependencies[node]]
-        has_run: Set[Node] = set()
+        in_flight: Dict[Node, Greenlet] = {}
 
-        def _mark_done(node: Node[NodeVal], value: NodeVal):
-            return_values[node] = value
-            has_run.add(node)
+        topological_ordering = self.graph.topological_ordering()
+        i = 0
+        while i < len(topological_ordering) and not in_flight:
+            head = topological_ordering[i]
 
-            to_remove = set()
-            for node in not_run:
-                if len(graph.dependencies[node] & has_run) == len(graph.dependencies[node]):
-                    to_remove.add(node)
-                    can_run.append(node)
+            # collect any other nodes that haven't finished yet
+            missing_dependencies: Set[Greenlet] = set()
+            for dependency in self.graph.edges[head]:
+                if dependency not in return_values:
+                    if dependency not in in_flight:
+                        # TODO(crockeo): raise better
+                        raise Exception()
+                    missing_dependencies.add(in_flight[dependency])
 
-            for node in to_remove:
-                not_run.discard(node)
+            # wait for all missing deps
+            for missing_dependency in missing_dependencies:
+                missing_dependency.join()
 
-        def _make_node_wrapper(node: Node) -> Callable[..., T]:
-            def _node_wrapper(*args, **kwargs) -> T:
-                ret = node(*args, **kwargs)
-                _mark_done(node, ret)
-                return ret
+            # collect kwargs
+            kwargs = {
+                dependency.name: return_values[dependency] for dependency in self.graph.edges[head]
+            }
 
-            return _node_wrapper
+            # launch it!!
+            pool.wait_available()
+            in_flight[head] = pool.spawn(
+                self._make_node_wrapper(head, return_values, in_flight),
+                **kwargs,
+            )
 
-        while not_run or can_run:
-            if not can_run:
-                self.pool.join()
-                continue
+        return return_values[self.terminal_node]
 
-            node = can_run.pop()
+    def _make_node_wrapper(
+        self,
+        node: Node[U],
+        return_values: Dict[Node, Any],
+        in_flight: Dict[Node, Greenlet],
+    ) -> Callable[..., U]:
+        """
+        Produces a Callable wrapper around a Node to perform clean-up and earmarking after it's
+        done executing.
+        """
 
-            node_wrapper = _make_node_wrapper(node)
-            kwargs = {}
-            for dependency in graph.dependencies[node]:
-                kwargs[dependency.name] = return_values[dependency]
+        def _node_wrapper(*args, **kwargs) -> U:
+            ret = node(*args, **kwargs)
+            return_values[node] = ret
+            # aggressively throwing now so we can see if this edge case every comes up
+            in_flight.pop(node)
+            return ret
 
-            self.pool.wait_available()
-            self.pool.spawn(node_wrapper, **kwargs)
-
-        self.pool.join()
-
-        return return_values[graph.terminal_node]
+        return _node_wrapper
